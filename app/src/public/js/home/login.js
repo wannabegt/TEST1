e49dd8dbc0e9120dc64afe924ae2ef66b0628313
soojung <wannabeda@hanmail.net>
2022-12-18 14:30:25 +0900

    로그인을 요청하는 fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,26 @@
 "use strict";
 
-console.log('hello');
-console.log('bye');
+const { application } = require("express");
+
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req ={
+        id : id.value,
+        psword : psword.value,
+    };
+
+    //데이터 전달
+    fetch("/login", {
+        method: "POST",
+        header:{
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
+    
+}
